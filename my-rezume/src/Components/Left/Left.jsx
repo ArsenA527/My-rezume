0596d01d95a7f291b82396fe4fe3871abd689efa
Arsen <arsen.avetisyan.2000@mail.ru>
2021-09-24 11:25:23 +0300

    make me

--- my-rezume/src/Components/Left/Left.jsx
+++ my-rezume/src/Components/Left/Left.jsx
@@ -1,19 +1,13 @@
 import React from 'react';
 import styles from './Left.module.css';
+import Me from './Me/Me.jsx';
 
 const Left = () => {
   return (
     <div className={styles.left}>
-      <div className={styles.me}>
-        <div className={styles.me__photo}>
-        <img src="https://sun9-82.userapi.com/impg/KH2Y5r9_OykBHygiq_8MKLG1RXz7-PHl3tIbGQ/w2cNyG8HWTg.jpg?size=1197x1600&quality=96&sign=5e3a4cbd937acd067bb30cc7d7251814&type=album" alt="My-photo" />
-        </div>
-        <div className={styles.me__desc}>
-          <h1 className={styles.me__fullname}>Арсен Мелконян</h1>
-          <h3 className={styles.my__proffesion}>Веб-разработчик</h3>
-        </div>
+      <Me />
+      <div className="contact">
       </div>
-
       {/* <div className={contact}> 
         <div className={address}></div>
         <div className={dateOfBirth}></div>
--- my-rezume/src/Components/Right/Right.jsx
+++ my-rezume/src/Components/Right/Right.jsx
@@ -6,13 +6,28 @@ const Right = () => {
   return (
     <div className="right">
       <div className="profile">
-        <h1>profile</h1>
+        <p className="desc">
+        Начинающий Front-End разработчик. Занимаюсь разработкой сайтов в течение 1 года. Есть цель:
+        развитие и построение карьеры в данной сфере.
+        </p>
       </div>
       <div className="achievements">
-        <h1>achievements</h1>
+  
+        
       </div>
       <div className="education">
-        <h1>education</h1>
+        
+        <div class="my_education">
+          <h4>Национальный исследовательский университет «Московский институт электронной техники»</h4>
+          <p>Направление: Управление в технических системах</p>
+          <span>Сентябрь 2018 - н.в. <b>|</b> Россия</span>
+        </div>
+
+        <h3>Дополнительное Образование</h3>
+        <div class="my_education">
+          <h4>Центр Компьютерного обучения и дополнительного образования</h4>
+          <span>Октябрь 2014 - Май 2017 <b>|</b> Россия</span>
+        </div>
       </div>
     </div>
   )
